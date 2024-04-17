cf4e04b408e63821f601d7005f79ef6b6c1881e4
Saheed Adeniji <adeniji4success@gmail.com>
2024-04-17 07:56:38 +0100

    Add project files.

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+public class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
+{
+    //public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : base(options)
+    //{
+            
+    //}
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+    }
+}
