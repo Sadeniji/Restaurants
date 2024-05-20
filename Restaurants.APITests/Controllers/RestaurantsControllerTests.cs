ed3629390a270b1ffed0057bd6dd435e7e5a47e9
Saheed Adeniji <adeniji4success@gmail.com>
2024-05-20 06:41:49 +0100

    Add integration testing for with testing endpoint with authentication and authorization

---
+++ Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Restaurants.APITests;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _applicationFactory;
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+
+    public RestaurantsControllerTests(WebApplicationFactory<Program> applicationFactory)
+    {
+        _applicationFactory = applicationFactory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                    _ => _restaurantsRepositoryMock.Object));
+            });
+        });
+    }
+
+    [Fact()]
+    public async Task GetAll_ForValidRequest_Returns200k()
+    {
+        // Arrange
+        var client = _applicationFactory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants?pageNumber=1&pageSize=10");
+
+        // assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact()]
+    public async Task GetAll_ForInvalidRequest_Returns400BadRequest()
+    {
+        // Arrange
+        var client = _applicationFactory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants");
+
+        // assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact()]
+    public async Task GetById_ForNonExistingId_ShouldReturns404NotFound()
+    {
+        // Arrange
+        var id = 1123;
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id, CancellationToken.None))
+            .ReturnsAsync((Restaurant?)null);
+
+        var client = _applicationFactory.CreateClient();
+
+        // act
+        var result = await client.GetAsync($"/api/restaurants/{id}");
+
+        // assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    //[Fact()]
+    //public async Task GetById_ForExistingId_ShouldReturns200Ok()
+    //{
+    //    // Arrange
+    //    var id = 99;
+
+    //    var restaurant = new Restaurant()
+    //    {
+    //        Id = id,
+    //        Name = "Test",
+    //        Description = "Test description"
+    //    };
+
+    //    _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id, CancellationToken.None))
+    //        .ReturnsAsync(restaurant);
+
+    //    var client = _applicationFactory.CreateClient();
+
+    //    // act
+    //    var result = await client.GetAsync($"/api/restaurants/{id}");
+    //    var restaurantDto = await result.Content.ReadFromJsonAsync<RestaurantDto>();
+
+    //    // assert
+    //    result.StatusCode.Should().Be(HttpStatusCode.OK);
+    //    restaurantDto.Should().NotBeNull();
+    //    restaurantDto.Name.Should().Be("Test");
+    //}
+}
---
+++ Restaurants.APITests/Middlewares/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+using Xunit;
+using FluentAssertions;
+
+namespace Restaurants.API.Middlewares.Tests;
+
+public class ErrorHandlingMiddlewareTests
+{
+    [Fact()]
+    public async Task InvokeAsync_WhenNoExceptionThrown_ShouldCallNextDelegate()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+
+        // act
+        await middleWare.InvokeAsync(context, nextDelegateMock.Object);
+
+        // assert
+        nextDelegateMock.Verify(next => next.Invoke(context), Times.Once);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCodeTo404()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var notFoundException = new NotFoundException(nameof(Restaurant), "1");
+
+        // act
+        await middleWare.InvokeAsync(context, _ => throw notFoundException);
+
+        // assert
+        context.Response.StatusCode.Should().Be(404);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCodeTo403()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var exception = new ForbiddenException();
+
+        // act
+        await middleWare.InvokeAsync(context, _ => throw exception);
+
+        // assert
+        context.Response.StatusCode.Should().Be(403);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCodeTo500()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var exception = new Exception();
+
+        // act
+        await middleWare.InvokeAsync(context, _ => throw exception);
+
+        // assert
+        context.Response.StatusCode.Should().Be(500);
+    }
+}
